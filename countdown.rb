45798b5e2b3e25d971ed864ad1b2846370b6a751
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-14 20:18:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,13 @@
 def countdown(n)
   while n > 0
+   puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  while n > 0
    sleep 1
    puts "#{n} SECOND(S)!"
     n -= 1
