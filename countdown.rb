ba5186193f5f0eff55fd23d38f1b2f51d77fff11
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-14 20:18:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(n)
+  while n > 0
+   puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  while n > 0
+   sleep 1
+   puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
